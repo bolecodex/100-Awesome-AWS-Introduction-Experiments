b83518e0305f7d250601596f561070e0f3d9c4c7
Weibo Zhao <bolecodex@gmail.com>
2022-06-26 14:06:50 +0200

    add pipeline

--- CDK for Java/cdk-workshop/src/main/java/com/myorg/WorkshopPipelineStack.java
+++ CDK for Java/cdk-workshop/src/main/java/com/myorg/WorkshopPipelineStack.java
@@ -5,6 +5,9 @@ import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 
 import software.amazon.awscdk.services.codecommit.Repository;
+import software.amazon.awscdk.pipelines.CodeBuildStep;
+import software.amazon.awscdk.pipelines.CodePipeline;
+import software.amazon.awscdk.pipelines.CodePipelineSource;
 
 public class WorkshopPipelineStack extends Stack {
     public WorkshopPipelineStack(final Construct parent, final String id) {
@@ -19,5 +22,18 @@ public class WorkshopPipelineStack extends Stack {
         final Repository repo = Repository.Builder.create(this, "WorkshopRepo")
             .repositoryName("WorkshopRepo")
             .build();
+                    // The basic pipeline declaration. This sets the initial structure
+        // of our pipeline
+        final CodePipeline pipeline = CodePipeline.Builder.create(this, "Pipeline")
+        .pipelineName("WorkshopPipeline")
+        .synth(CodeBuildStep.Builder.create("SynthStep")
+                .input(CodePipelineSource.codeCommit(repo, "main"))
+                .installCommands(List.of(
+                        "npm install -g aws-cdk"   // Commands to run before build
+                ))
+                .commands(List.of("mvn package",            // Language-specific build commands
+                        "npx cdk synth"           // Synth command (always same)
+                )).build())
+        .build();
     }
 }
