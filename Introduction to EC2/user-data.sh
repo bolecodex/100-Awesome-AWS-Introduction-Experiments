9b6e5bfaf63efabc0c307bc72cd7c5ce07eec678
Weibo Zhao <94233933+bolecodex@users.noreply.github.com>
2024-03-04 21:39:43 +0100

    Update user-data.sh

--- Introduction to EC2/user-data.sh
+++ Introduction to EC2/user-data.sh
@@ -5,5 +5,6 @@ systemctl start httpd
 systemctl enable httpd
 cd /var/www/html
 aws s3 cp s3://aws-weibo/index.txt ./
-EC2AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone) 
-sed 's/INSTANCEID/$EC2AZ/' index.txt > index.html
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` 
+EC2AZ=$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone -H "X-aws-ec2-metadata-token: $TOKEN") 
+sed "s/INSTANCEID/$EC2AZ/" index.txt > index.html
