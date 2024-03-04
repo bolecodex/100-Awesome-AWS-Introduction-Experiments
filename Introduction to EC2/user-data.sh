f51c4f8889bd3ec179773016ab65dd61a09c261c
Weibo Zhao <94233933+bolecodex@users.noreply.github.com>
2024-03-04 21:06:02 +0100

    Update user-data.sh

--- Introduction to EC2/user-data.sh
+++ Introduction to EC2/user-data.sh
@@ -4,6 +4,6 @@ yum install httpd -y
 systemctl start httpd
 systemctl enable httpd
 cd /var/www/html
-aws s3 cp s3://web-wang/index.txt ./
+aws s3 cp s3://aws-weibo/index.txt ./
 EC2AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone) 
 sed "s/INSTANCEID/$EC2AZ/" index.txt > index.html
